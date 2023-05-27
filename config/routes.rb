a6d829f7876f731bf0ed1bfa3ad94a5536b8606c
linh <klinh2212112@gmail.com>
2023-05-27 11:02:05 +0700

    set authorization final

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :books
   # get 'home/index'
   get 'home/about'
-  root 'home#index'
+  # root 'home#index'
+  root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
